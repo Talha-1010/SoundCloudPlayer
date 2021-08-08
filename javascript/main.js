4998df770458111ef5e8ca31e2cdc2577aeccefe
Talha-1010 <talhanasr3e@gmail.com>
2021-08-09 02:49:04 +0500

    added JQuery empty() method

--- javascript/main.js
+++ javascript/main.js
@@ -2,71 +2,76 @@
 
 
 
+var searchButton =document.querySelector('i');
+searchButton.addEventListener('click',function(){
+    var input = document.querySelector('input').value;
+    getTracks(input);
+
+});
+
 
 
 SC.initialize({
   client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
 });
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'buskers'
-}).then(function(tracks) {
-    console.log(tracks);
-    tracks.forEach(track => {
-        
-        var card = document.createElement('div');
-        card.classList.add("card");
 
-        var imageDiv = document.createElement('div');
-        var img = document.createElement('img');
-        img.classList.add("image_img");
-        img.src=track.artwork_url || "https://picsum.photos/290/290";
-        imageDiv.appendChild(img);
-        card.appendChild(img);
+function getTracks(name){
+    SC.get('/tracks', {
+        q: name
+      }).then(function(tracks) {
+          console.log(tracks);
+          $("#searchResults").empty();
 
-        var contentDiv = document.createElement('div');
-        contentDiv.className="content";
-
-        var header = document.createElement('div');
-        header.className="header";
-
-        var hyperLink = document.createElement('a');
-        hyperLink.href=track.permalink_url;
-        hyperLink.target="_blank";
-        hyperLink.innerText= track.title;
-
-        header.appendChild(hyperLink);
-        contentDiv.appendChild(header);
-        card.appendChild(contentDiv);
-
-
-        var button = document.createElement('div');
-        button.className = "ui bottom attached button js-button";
-
-        var icon = document.createElement("i");
-        icon.className="add icon";
-
-        var span = document.createElement('span');
-        span.innerText = "Add to playlist";
-
-        button.appendChild(icon);
-        button.appendChild(span);
-
-        card.appendChild(button);
-
-        var searchResults = document.getElementById("searchResults");
-        searchResults.appendChild(card);
-
-    });
-
-
-
-});
-
-
-
-
+          tracks.forEach(track => {
+              
+              var card = document.createElement('div');
+              card.classList.add("card");
+      
+              var imageDiv = document.createElement('div');
+              var img = document.createElement('img');
+              img.classList.add("image_img");
+              img.src=track.artwork_url || "https://picsum.photos/290/290";
+              imageDiv.appendChild(img);
+              card.appendChild(img);
+      
+              var contentDiv = document.createElement('div');
+              contentDiv.className="content";
+      
+              var header = document.createElement('div');
+              header.className="header";
+      
+              var hyperLink = document.createElement('a');
+              hyperLink.href=track.permalink_url;
+              hyperLink.target="_blank";
+              hyperLink.innerText= track.title;
+      
+              header.appendChild(hyperLink);
+              contentDiv.appendChild(header);
+              card.appendChild(contentDiv);
+      
+      
+              var button = document.createElement('div');
+              button.className = "ui bottom attached button js-button";
+      
+              var icon = document.createElement("i");
+              icon.className="add icon";
+      
+              var span = document.createElement('span');
+              span.innerText = "Add to playlist";
+      
+              button.appendChild(icon);
+              button.appendChild(span);
+      
+              card.appendChild(button);
+      
+              var searchResults = document.getElementById("searchResults");
+              searchResults.appendChild(card);
+      
+          });
+      
+      });
+}
 
 
 })();
